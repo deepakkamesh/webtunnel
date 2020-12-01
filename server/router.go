9a634cb7851d617536a45ccc24db2eed02f66b6b
Deepak Guruswamy <deepak.kamesh@gmail.com>
2020-11-30 18:59:02 -0800

    minor changes

--- server/router.go
+++ server/router.go
@@ -3,13 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
-	"net"
 	"os/exec"
-	"time"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
-	"github.com/google/gopacket/pcap"
 	"github.com/gorilla/websocket"
 	"github.com/songgao/water"
 )
@@ -17,17 +14,11 @@ import (
 type Router struct {
 	WSData   chan WSPkt                 // Channel to receive data from websocket.
 	Conns    map[string]*websocket.Conn // Websocket connection.
-	ethIface *pcap.Handle               // Handle for routeable network interface.
 	tunIface *water.Interface           // Handle to the TUN interface.
+	quit     chan struct{}              // Channel to handle shutdown.
 }
 
-func NewRouter(ethIfce string) (*Router, error) {
-
-	// Open handle to routable network interface on host.
-	handle, err := pcap.OpenLive(ethIfce, 1024, false, 30*time.Second)
-	if err != nil {
-		return nil, err
-	}
+func NewRouter(tunIP, tunNetmask string) (*Router, error) {
 
 	// Create TUN interface.
 	tunIfce, err := water.New(
@@ -40,14 +31,13 @@ func NewRouter(ethIfce string) (*Router, error) {
 
 	// Assign IP to TUN.
 	// TODO: Handle other Operating Systems and add routing for network prefixs.
-	cmd := exec.Command("/sbin/ifconfig", tunIfce.Name(), "10.0.0.1", "netmask", "255.255.255.0", "up")
+	cmd := exec.Command("/sbin/ifconfig", tunIfce.Name(), tunIP, "netmask", tunNetmask, "up")
 	if err := cmd.Run(); err != nil {
 		return nil, fmt.Errorf("error setting ip on tun %s", err)
 	}
 	return &Router{
 		WSData:   make(chan WSPkt, 10),
 		Conns:    make(map[string]*websocket.Conn),
-		ethIface: handle,
 		tunIface: tunIfce,
 	}, nil
 }
@@ -65,7 +55,7 @@ func (r *Router) Start() {
 				gopacket.Default,
 			))
 
-			if err := sendNet(data.payload, r.ethIface); err != nil {
+			if err := sendNet(data.payload, r.tunIface); err != nil {
 				log.Fatalf("Error processing packet from WS %s", err)
 			}
 		}
@@ -117,7 +107,7 @@ func sendWS(pkt []byte, conn *websocket.Conn) error {
 	return nil
 }
 
-func sendNet(pkt []byte, handle *pcap.Handle) error {
+func sendNet(pkt []byte, handle *water.Interface) error {
 
 	packet := gopacket.NewPacket(pkt, layers.LayerTypeIPv4, gopacket.Default)
 
@@ -125,68 +115,44 @@ func sendNet(pkt []byte, handle *pcap.Handle) error {
 
 	opts := gopacket.SerializeOptions{FixLengths: true, ComputeChecksums: true}
 
-	// net.HardwareAddr{0x00,0x23,0x4d,0x09,0xfd,0x92} win
-	//  net.HardwareAddr{0x80, 0xE6, 0x50, 0x18, 0x9B, 0xBA} Mac
-	// net.HardwareAddr{0x18, 0x3D, 0xA2, 0x42, 0xE4, 0x4C}, lin
-	ethl := &layers.Ethernet{
-		SrcMAC:       net.HardwareAddr{0x18, 0x3D, 0xA2, 0x42, 0xE4, 0x4C},
-		DstMAC:       net.HardwareAddr{0x00, 0x23, 0x4d, 0x09, 0xfd, 0x92},
-		EthernetType: layers.EthernetTypeIPv4,
-	}
-
-	ipl := &layers.IPv4{
-		SrcIP:      ip.SrcIP, // net.IP{10, 2, 0, 1},// Source should change. TODO
-		DstIP:      ip.DstIP,
-		Protocol:   ip.Protocol,
-		Version:    ip.Version,
-		IHL:        ip.IHL,
-		TOS:        ip.TOS,
-		Id:         ip.Id,
-		Flags:      ip.Flags,
-		FragOffset: ip.FragOffset,
-		TTL:        ip.TTL,
-		Options:    ip.Options,
-		Padding:    ip.Padding,
-	}
-
 	buffer := gopacket.NewSerializeBuffer()
 
 	switch ip.NextLayerType() {
 	case layers.LayerTypeUDP:
 		trans := packet.Layer(layers.LayerTypeUDP).(*layers.UDP)
-		if err := trans.SetNetworkLayerForChecksum(ipl); err != nil {
+		if err := trans.SetNetworkLayerForChecksum(ip); err != nil {
 			return fmt.Errorf("error checksum %s", err)
 		}
-		if err := gopacket.SerializeLayers(buffer, opts, ethl, ipl, trans, gopacket.Payload(trans.Payload)); err != nil {
+		if err := gopacket.SerializeLayers(buffer, opts, ip, trans, gopacket.Payload(trans.Payload)); err != nil {
 			return fmt.Errorf("error serializelayer %s", err)
 		}
 
 	case layers.LayerTypeTCP:
 		trans := packet.Layer(layers.LayerTypeTCP).(*layers.TCP)
-		if err := trans.SetNetworkLayerForChecksum(ipl); err != nil {
+		if err := trans.SetNetworkLayerForChecksum(ip); err != nil {
 			return fmt.Errorf("error checksum %s", err)
 		}
 
-		trans.SetNetworkLayerForChecksum(ipl)
-		if err := gopacket.SerializeLayers(buffer, opts, ethl, ipl, trans, gopacket.Payload(trans.Payload)); err != nil {
+		trans.SetNetworkLayerForChecksum(ip)
+		if err := gopacket.SerializeLayers(buffer, opts, ip, trans, gopacket.Payload(trans.Payload)); err != nil {
 			return fmt.Errorf("error serializelayer %s", err)
 		}
 
 	case layers.LayerTypeICMPv4:
 		trans := packet.Layer(layers.LayerTypeICMPv4).(*layers.ICMPv4)
-		if err := gopacket.SerializeLayers(buffer, opts, ethl, ipl, trans, gopacket.Payload(trans.Payload)); err != nil {
+		if err := gopacket.SerializeLayers(buffer, opts, ip, trans, gopacket.Payload(trans.Payload)); err != nil {
 			return fmt.Errorf("error serializelayer %s", err)
 		}
 	}
 
-	fmt.Println("send to Eth", gopacket.NewPacket(
+	fmt.Println("send to Tun", gopacket.NewPacket(
 		buffer.Bytes(),
 		layers.LayerTypeEthernet,
 		gopacket.Default,
 	))
 
-	if err := handle.WritePacketData(buffer.Bytes()); err != nil {
-		return fmt.Errorf("error send %s", err)
+	if _, err := handle.Write(buffer.Bytes()); err != nil {
+		return fmt.Errorf("error sending to tun %s", err)
 	}
 	return nil
 }
--- server/webtunnel_server.go
+++ server/webtunnel_server.go
@@ -9,15 +9,13 @@ func main() {
 
 	// Initialize the handlers/routers.
 	fmt.Printf("Starting WebTunnel Client")
-	router, err := NewRouter("wlo1")
+	router, err := NewRouter("10.0.0.1", "255.255.255.0")
 	if err != nil {
 		log.Fatalf("Failed to inialize Router %s", err)
 	}
-	wshandler := NewWSHandler("192.168.1.112:8811", router)
+	wshandler := NewWSHandler("192.168.1.117:8811", router)
 
 	// Start the servers
-	go wshandler.Start()
 	router.Start()
-	for {
-	}
+	wshandler.Start()
 }
