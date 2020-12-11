4fee18aa346a6530edcd841a6dab038e64f07f11
Deepak Guruswamy <deepak.kamesh@gmail.com>
2020-12-11 12:13:08 -0800

    added dnsforwarder support to server

--- examples/server.go
+++ examples/server.go
@@ -9,27 +9,27 @@ import (
 
 func main() {
 	// Get some flags.
-	/*	listenAddr := flag.String("listenAddr", "192.168.1.117:8811", "Bind address:port")
-		httpsKeyFile := flag.String("httpsKeyFile", "", "HTTPS Key file path")
-		httpsCertFile := flag.String("httpsCertFile", "", "HTTPS Cert file path")
+	listenAddr := flag.String("listenAddr", ":8811", "Bind address:port")
+	httpsKeyFile := flag.String("httpsKeyFile", "localhost.key", "HTTPS Key file path")
+	httpsCertFile := flag.String("httpsCertFile", "localhost.crt", "HTTPS Cert file path")
 
-		flag.Parse()
-
-		glog.Info("starting webtunnel server..")
-			server, err := webtunnelserver.NewWebTunnelServer(*listenAddr, "192.168.0.1",
-				"255.255.255.0", "192.168.0.0/24", "172.16.0.0/24", *httpsKeyFile, *httpsCertFile)
-			if err != nil {
-				glog.Fatalf("%s", err)
-			}
-			server.Start()*/
 	flag.Parse()
-	dns, err := webtunnelserver.NewDNSForwarder("192.168.1.117", 53)
+
+	glog.Info("starting webtunnel server..")
+	server, err := webtunnelserver.NewWebTunnelServer(*listenAddr, "192.168.0.1",
+		"255.255.255.0", "192.168.0.0/24", "172.16.0.0/24", *httpsKeyFile, *httpsCertFile)
+	if err != nil {
+		glog.Fatalf("%s", err)
+	}
+	server.Start()
+
+	glog.Info("starting DNS Forwarder..")
+	dns, err := webtunnelserver.NewDNSForwarder("192.168.0.1", 53)
 	if err != nil {
 		glog.Fatal(err)
 	}
-	if err := dns.Start(); err != nil {
-		glog.Fatal(err)
-	}
+	dns.Start()
+
 	for {
 	}
 }
--- webtunnelserver/dnsforwarder.go
+++ webtunnelserver/dnsforwarder.go
@@ -25,9 +25,8 @@ func NewDNSForwarder(ip string, port int) (*DNSForwarder, error) {
 	}, nil
 }
 
-func (d *DNSForwarder) Start() error {
+func (d *DNSForwarder) Start() {
 	go d.listenServ()
-	return nil
 }
 
 func (d *DNSForwarder) listenServ() {
@@ -36,10 +35,11 @@ func (d *DNSForwarder) listenServ() {
 
 		_, peerAddr, err := d.handle.ReadFrom(pkt)
 		if err != nil {
-			glog.Error("error reading from net %v", err)
+			glog.Errorf("error reading from net %v", err)
 			return
 		}
 
+		// Verify if packet is valid DNS request.
 		dnsReq, ok := gopacket.NewPacket(pkt, layers.LayerTypeDNS, gopacket.Default).Layer(layers.LayerTypeDNS).(*layers.DNS)
 		if !ok {
 			glog.Warning("Not a valid DNS request")
@@ -49,33 +49,31 @@ func (d *DNSForwarder) listenServ() {
 		hostname := string(dnsReq.Questions[0].Name)
 		glog.Infof("Got from %v name resolution for %v", peerAddr, hostname)
 
-		resp := &layers.DNS{}
-
+		// Only respond for support use cases.
 		if err := validateReq(dnsReq); err != nil {
 			glog.Warning("DNS request not supported")
-			resp = buildResponse(dnsReq, nil, layers.DNSResponseCodeNotImp)
+			if err := d.sendResponse(dnsReq, peerAddr, nil, layers.DNSResponseCodeNotImp); err != nil {
+				glog.Errorf("Error sending DNS response %v", err)
+				return
+			}
 			continue
 		}
 
+		// Try to lookup hostname.
 		ips, err := net.LookupHost(hostname)
-
 		if err != nil {
-			glog.Warning("Unable to resolve %s", hostname)
-			resp = buildResponse(dnsReq, nil, layers.DNSResponseCodeNotZone)
+			glog.Warningf("Unable to resolve %v", hostname)
+			if err := d.sendResponse(dnsReq, peerAddr, nil, layers.DNSResponseCodeNXDomain); err != nil {
+				glog.Errorf("Error sending DNS response %v", err)
+				return
+			}
 			continue
 		}
 
 		// All ok, build and send response.
-		resp = buildResponse(dnsReq, ips, layers.DNSResponseCodeNoErr)
-
-		buff := gopacket.NewSerializeBuffer()
-		opts := gopacket.SerializeOptions{FixLengths: true, ComputeChecksums: true}
-		if err := resp.SerializeTo(buff, opts); err != nil {
-			glog.Error("Error serializing %v", err)
-		}
-
-		if _, err := d.handle.WriteTo(buff.Bytes(), peerAddr); err != nil {
-			glog.Errorf("Error writing response %v", err)
+		if err := d.sendResponse(dnsReq, peerAddr, ips, layers.DNSResponseCodeNoErr); err != nil {
+			glog.Errorf("Error sending DNS response %v", err)
+			return
 		}
 	}
 }
@@ -87,9 +85,10 @@ func validateReq(req *layers.DNS) error {
 	return fmt.Errorf("invalid request")
 }
 
-func buildResponse(req *layers.DNS, ips []string, respCode layers.DNSResponseCode) *layers.DNS {
+func (d *DNSForwarder) sendResponse(req *layers.DNS, peerAddr net.Addr, ips []string, respCode layers.DNSResponseCode) error {
 
 	answers := []layers.DNSResourceRecord{}
+	ancount := 0
 
 	// Build answer struct for range of IPs.
 	for _, v := range ips {
@@ -98,31 +97,47 @@ func buildResponse(req *layers.DNS, ips []string, respCode layers.DNSResponseCod
 			glog.Errorf("Unable to parse address %v", err)
 			continue
 		}
+		// Return only IPv4 answers.
+		if ip.To4() == nil {
+			continue
+		}
 		answers = append(answers,
 			layers.DNSResourceRecord{
 				Name:  []byte(req.Questions[0].Name),
 				Type:  layers.DNSTypeA,
 				Class: layers.DNSClassIN,
-				TTL:   600,
+				TTL:   4,
 				IP:    ip,
 			})
+		ancount++
 	}
 
 	dns := layers.DNS{
-		ID:     req.ID,
-		QR:     true,
-		OpCode: req.OpCode,
+		ID:     req.ID,     // Request ID; returned as is in response.
+		QR:     true,       // Query Response flag.
+		OpCode: req.OpCode, // OPCode; returned as is in response.
 
-		AA: false,
-		TC: false,
-		RD: req.RD,
-		RA: false,
-		Z:  0,
+		AA: false,  // Authoritative Answer.
+		TC: false,  // Truncation flag.
+		RD: req.RD, // Recursion Desired.
+		RA: false,  // Recursion Available.
+		Z:  0,      // Reserved.
 
 		ResponseCode: respCode,
-		ANCount:      1,
+		ANCount:      uint16(ancount),
 		Answers:      answers,
 	}
 
-	return &dns
+	// Send Response.
+	buff := gopacket.NewSerializeBuffer()
+	opts := gopacket.SerializeOptions{FixLengths: true, ComputeChecksums: true}
+	if err := dns.SerializeTo(buff, opts); err != nil {
+		return fmt.Errorf("Error serializing DNS response %v", err)
+	}
+
+	if _, err := d.handle.WriteTo(buff.Bytes(), peerAddr); err != nil {
+		return fmt.Errorf("Error writing response to interface %v", err)
+	}
+
+	return nil
 }
