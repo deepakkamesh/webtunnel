65043c7f1f28409c2e6a380cfa313bf6a6386552
Deepak Guruswamy <deepak.kamesh@gmail.com>
2021-01-01 10:33:03 -0800

    updated config application

--- webtunnelclient/webtunnelclient_darwin.go
+++ webtunnelclient/webtunnelclient_darwin.go
@@ -6,22 +6,21 @@ import (
 )
 
 // initializeTunnel assigns IP to tunnel and sets up routing via tunnel.
-func SetIP(ipLocal, ipGW, ifceName string) error {
+func SetIP(cfg *InterfaceCfg, ifceName string) error {
 	cmd := exec.Command("/sbin/ifconfig", ifceName, "down")
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("error setting ip on tun %s", err)
 	}
-	cmd = exec.Command("/sbin/ifconfig", ifceName, ipLocal, ipGW, "up")
+	cmd = exec.Command("/sbin/ifconfig", ifceName, cfg.IP, cfg.GWIP, "up")
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("error setting ip on tun %s", err)
 	}
-	return nil
-}
 
-func SetRoute(routePrefix, ifceName string) error {
-	cmd := exec.Command("/sbin/route", "-n", "add", "-net", routePrefix, "-interface", ifceName)
-	if err := cmd.Run(); err != nil {
-		return fmt.Errorf("error setting ip on tun %s", err)
+	for _, route := range cfg.RoutePrefix {
+		cmd := exec.Command("/sbin/route", "-n", "add", "-net", route, "-interface", ifceName)
+		if err := cmd.Run(); err != nil {
+			return fmt.Errorf("error setting route on tun %s", err)
+		}
 	}
 	return nil
 }
